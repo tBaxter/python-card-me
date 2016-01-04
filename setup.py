2864433f5fdc8ed6844f2ca4d1c7aac56eb272a0
TB026891 <tim.baxter@cerner.com>
2016-01-04 14:29:27 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,15 @@
-"""VObject: module for reading vCard and vCalendar files
+"""
 
-Description
------------
 
-Parses iCalendar and vCard files into Python data structures, decoding the relevant encodings.
-Also serializes vobject data structures to iCalendar, vCard, or (experimentally)
-hCalendar unicode strings.
+Python-Card-Me parses iCalendar and vCard files into Python data structures, decoding the relevant encodings.
+It can also serialize vobject data structures to iCalendar, vCard, or hCalendar unicode strings.
+
+It is a substantially reworked version of Vobject, updated for python3, with other improvements.
 
 Requirements
 ------------
 
-Requires python 2.4 or later, dateutil (http://labix.org/python-dateutil) 1.1 or later.
-
-Recent changes
---------------
-   - Make change_tz.py compatible with python 2.4, so the entire package stays compatible
-   - Fall back to default (the most recent standard) behavior if a VCARD
-     or VCALENDAR doesn't have a recognized VERSION
-   - Fixed a bad performance bug when parsing large text bodies, thanks to Morgen Sagen at Apple
-   - Changed license to Apache 2.0 from Apache 1.1
-   - Worked around an issue with Apple Address Book's vcard PHOTO parser
-   - Added change_tz module and script for quickly changing event timezones for an
-     ics file.  Requires PyICU.
-   - Add support for BYMONTHDAY=-1 (days before the end of the month) when setting rrules
-     from a dateutil rrule
-   - Tolerate a Ruby iCalendar library escaping semi-colons in RRULEs
-   - Make vobjects pickle-able
-   - Add introspection help for IPython so tab completion works with
-     vobject's custom __getattr__
-   - Allow Outlook's technically illegal use of commas in TZIDs
-   - Allow unicode names for TZIDs
-   - Worked around Lotus Notes use of underscores in names by just silently replacing
-     with dashes
-   - When allowing quoted-printable data, honor CHARSET for each line, defaulting to
-     iso-8859-1
-   - Simplified directory layout, unit tests are now available via setup.py test
+Requires Python 2.7 or later, dateutil (http://labix.org/python-dateutil) 2.4 or later, and six.
 
 For older changes, see
    - http://vobject.skyhouseconsulting.com/history.html or
@@ -46,17 +21,17 @@ from setuptools import setup, find_packages
 
 doclines = __doc__.splitlines()
 
-setup(name="vobject2",
-      version="0.8.6",
-      author="Jeffrey Harris, Tim Baxter",
+setup(name="python-card-me",
+      version="0.9.0,
+      author="Tim Baxter, Jeffrey Harris",
       author_email="mail.baxter@gmail.com",
       license="Apache",
       zip_safe=True,
-      url="http://vobject.skyhouseconsulting.com",
+      url="",
       entry_points={
           'console_scripts': [
-              'ics_diff = vobject.ics_diff:main',
-              'change_tz = vobject.change_tz:main'
+              'ics_diff = card_me.ics_diff:main',
+              'change_tz = card_me.change_tz:main'
           ]
       },
       include_package_data=True,
