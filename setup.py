527c34616f9b25fcee49147d1d53f9074670fbc5
David Sanders <dsanders11@ucsbalum.com>
2016-12-03 02:55:40 -0800

    Add Python version classifiers to setup.py

--- setup.py
+++ setup.py
@@ -50,6 +50,11 @@ setup(name="python-card-me",
           Intended Audience :: Developers
           Natural Language :: English
           Programming Language :: Python
+          Programming Language :: Python :: 2
+          Programming Language :: Python :: 2.7
+          Programming Language :: Python :: 3
+          Programming Language :: Python :: 3.4
+          Programming Language :: Python :: 3.5
           Operating System :: OS Independent
           Topic :: Text Processing""".strip().splitlines()
       )
